af788ee5ddaebb46bada746376cc4d2d17a8249e
ruslanhutaba-sudo <ruslanhutaba@gmail.com>
2026-07-12 12:36:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import flet as ft
 import sympy as sp
 
 def main(page: ft.Page):
-    page.title = "Инженерный калькулятор"
+    page.title = "Калькулятор"
     page.theme_mode = ft.ThemeMode.DARK
     page.bgcolor = "#17171c"
 
@@ -13,7 +13,7 @@ def main(page: ft.Page):
         text_align=ft.TextAlign.RIGHT,
         read_only=True,
         border=ft.InputBorder.NONE,
-        text_size=42,
+        text_size=38,
         color="white"
     )
 
@@ -53,31 +53,30 @@ def main(page: ft.Page):
 
         page.update()
 
-    def btn(text, bg_color="#212124", text_color="white", arrow=False):
+    def btn(text, bg_color="#212124", text_color="white"):
         return ft.Container(
             content=ft.Text(text, color=text_color, weight=ft.FontWeight.BOLD, size=18),
             alignment=ft.alignment.center,
-            border_radius=30,
+            border_radius=10,
             bgcolor=bg_color,
             expand=True,
             on_click=button_click,
-            data=text,
-            animate=ft.Animation(300, "easeIn")
+            data=text
         )
 
     page.add(
         ft.Container(
             content=ft.Column([
                 ft.Container(content=txt_display, padding=10, expand=1),
-                ft.Row([btn("sin", "#2c2c35"), btn("cos", "#2c2c35"), btn("tan", "#2c2c35"), btn("ln", "#2c2c35"), btn("log", "#2c2c35")], spacing=10),
-                ft.Row([btn("π", "#2c2c35"), btn("e", "#2c2c35"), btn("√", "#2c2c35"), btn("^", "#2c2c35"), btn("(", "#2c2c35"), btn(")", "#2c2c35")], spacing=10),
-                ft.Row([btn("C", "#a5a5a5", "black"), btn("⌫", "#a5a5a5", "black"), btn("%", "#a5a5a5", "black"), btn("/", "#ff9f0a")], spacing=10),
-                ft.Row([btn("7"), btn("8"), btn("9"), btn("*", "#ff9f0a")], spacing=10),
-                ft.Row([btn("4"), btn("5"), btn("6"), btn("-", "#ff9f0a")], spacing=10),
-                ft.Row([btn("1"), btn("2"), btn("3"), btn("+", "#ff9f0a")], spacing=10),
-                ft.Row([btn("0", expand=2), btn("."), btn("=", "#ff9f0a")], spacing=10),
-            ], spacing=10, alignment=ft.MainAxisAlignment.END),
-            padding=20,
+                ft.Row([btn("sin", "#2c2c35"), btn("cos", "#2c2c35"), btn("tan", "#2c2c35"), btn("ln", "#2c2c35"), btn("log", "#2c2c35")], expand=True),
+                ft.Row([btn("π", "#2c2c35"), btn("e", "#2c2c35"), btn("√", "#2c2c35"), btn("^", "#2c2c35"), btn("(", "#2c2c35"), btn(")", "#2c2c35")], expand=True),
+                ft.Row([btn("C", "#a5a5a5", "black"), btn("⌫", "#a5a5a5", "black"), btn("%", "#a5a5a5", "black"), btn("/", "#ff9f0a")], expand=True),
+                ft.Row([btn("7"), btn("8"), btn("9"), btn("*", "#ff9f0a")], expand=True),
+                ft.Row([btn("4"), btn("5"), btn("6"), btn("-", "#ff9f0a")], expand=True),
+                ft.Row([btn("1"), btn("2"), btn("3"), btn("+", "#ff9f0a")], expand=True),
+                ft.Row([btn("0"), btn("."), btn("=", "#ff9f0a")], expand=True),
+            ], alignment=ft.MainAxisAlignment.END, expand=True),
+            padding=10,
             expand=True
         )
     )
